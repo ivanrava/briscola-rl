2120cdc3e5706732037e4afd9c9dbc766cc09de0
Ivan Ravasi <ivan4.ravasi@gmail.com>
2024-03-25 14:33:18 +0100

    More clever epsgreedyplayer

--- briscola_rl/players/epsgreedy_player.py
+++ briscola_rl/players/epsgreedy_player.py
@@ -25,13 +25,15 @@ class EpsGreedyPlayer(BasePlayer):
     def greedy_action(self, state: PublicState):
         im_first = state.order == self.order_idx
         if im_first:
-            return self.card_min_points()
+            return self.card_min_points(state)
         else:
             return self.card_max_gain(state)
 
     def card_max_gain(self, state: PublicState):
         i_max = -1
         max_gain = -100
+        i_max_b = -1
+        max_gain_b = -100
         table = state.table[:]
         table.append(None)
         for i, c in enumerate(self.hand):
@@ -39,17 +41,34 @@ class EpsGreedyPlayer(BasePlayer):
             winner = select_winner(table, state.briscola)
             coef_pts = 1 if winner else -1
             gain = coef_pts * sum(map(lambda card: card.points, table))
-            if gain > max_gain:
-                i_max = i
-                max_gain = gain
-        return i_max
+            if c.suit != state.briscola.suit:
+                if gain > max_gain:
+                    i_max = i
+                    max_gain = gain
+            else:
+                if gain > max_gain_b:
+                    i_max_b = i
+                    max_gain_b = gain
+        return i_max if max_gain >= 0 or max_gain_b < 0 else i_max_b
 
-    def card_min_points(self):
+    def card_min_points(self, state):
         # TODO: write a better code, I'm triggered
         i_min = -1
         min_pts = 1000
+        i_min_b = -1
+        min_pts_b = 1000
         for i, c in enumerate(self.hand):
-            if c.points < min_pts:
-                i_min = i
-                min_pts = c.points
-        return i_min
+            if c.suit != state.briscola.suit:
+                if c.points < min_pts:
+                    i_min = i
+                    min_pts = c.points
+                else:
+                    i_min_b = i
+                    min_pts_b = c.points
+
+        if min_pts <= 4:
+            return i_min
+        elif min_pts_b < 4 < min_pts:
+            return i_min_b
+        else:
+            return i_min
